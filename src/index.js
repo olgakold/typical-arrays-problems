02491a8d3f955a1d3e425b35d3ab96ce6798c2c2
olgakold <64192555+olgakold@users.noreply.github.com>
2020-09-30 20:06:09 +0300

    feat: add min, max ,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0){
+     return 0;
+   }
+   else{
+     return Math.min.apply (null, array)
+   } 
+  }
+  
+  exports.max = function max (array) {
+    if (array === undefined || array.length === 0){
+      return 0;
+    }
+    else{
+      return Math.max.apply (null, array)
+    } 
+  }
+  
+  exports.avg = function avg (array) {
+    if (array === undefined || array.length === 0){
+      return 0;
+    }
+    else{
+      var summ = 0;
+      array.map (i => summ = summ +i )
+      return summ/array.length
+    } 
+  }
+  
